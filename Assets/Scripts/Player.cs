0976325709ef367ccfca86f71dc82afb8f4a278f
Dan <me@danielholcomb.com>
2021-04-24 08:12:06 -0600

    Added follow point for fractional camera movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
     public float movementSpeed;
     public Vector3 dragPercent;
     public float maxFallSpeed;
-    public float recoilPercent;
+    public float minFallSpeed;
+    public float hangtime;
+    public Vector3 recoil;
     public Lasers lasers;
 
     private Rigidbody rb;
@@ -16,21 +18,27 @@ public class Player : MonoBehaviour
 
     private Vector3 tParam;
 
+    private bool hanging;
+
+    private float hangingTimer = 0;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        if(hangingTimer >= hangtime)
+        {
+            rb.useGravity = true;
+        }
+    }
     private void FixedUpdate()
     {
+        hangingTimer += Time.fixedDeltaTime;
         rb.AddForce((movement * movementSpeed));
-        rb.velocity = new Vector3(rb.velocity.x * dragPercent.x, rb.velocity.y, rb.velocity.z * dragPercent.z);
-        //rb.velocity = new Vector3(rb.velocity.x + (movement.x * movementSpeed), rb.velocity.y, rb.velocity.z + (movement.y * movementSpeed));
-
-        if(rb.velocity.y < maxFallSpeed)
-        {
-            rb.velocity = new Vector3(rb.velocity.x, maxFallSpeed, rb.velocity.z);
-        }
+        rb.velocity = new Vector3(rb.velocity.x * dragPercent.x, Mathf.Clamp(rb.velocity.y, maxFallSpeed, minFallSpeed), rb.velocity.z * dragPercent.z);
     }
 
     public void Movement(Vector2 inputMovement)
@@ -40,21 +48,8 @@ public class Player : MonoBehaviour
 
     public void Fire()
     {
-        Vector3 recoilCalc = new Vector3(0, rb.velocity.y*recoilPercent, 0);
-        rb.AddForce(recoilCalc);
+        rb.AddForce(recoil);
         lasers.Fire(new Vector3(GameManager.fireCursor.transform.position.x,GameManager.fireCursor.transform.position.y,0));//    Random.Range(0,300),Random.Range(0,300),0));
-    }
-
-    private void Recoil()
-    {
-        if(rb.velocity.y >= 0)
-        {
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z);
-        }
-        else
-        {
-            Vector3 recoilCalc = new Vector3(0, rb.velocity.y*recoilPercent, 0);
-        }
     }
 
 }
