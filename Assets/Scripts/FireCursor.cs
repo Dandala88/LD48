c852561bad3b757a7d5ad81128e1cce5453383c7
Brenden <brendenbunker@gmail.com>
2021-04-25 00:34:32 -0600

    Added mouse Sense and clamped cursor in window

--- Assets/Scripts/FireCursor.cs
+++ Assets/Scripts/FireCursor.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class FireCursor : MonoBehaviour
 {
-    public Canvas canvas;
+    private Canvas canvas;
+    public float mouseSensitivity = 1f;
 
     void Start() {
         canvas = GetComponentInParent<Canvas>();
     }
     public void Move(Vector2 inputMove)
     {
-        var newX = Mathf.Clamp(transform.position.x + inputMove.x,0,canvas.pixelRect.width);
-        var newY = Mathf.Clamp(transform.position.y + inputMove.y,0,canvas.pixelRect.height);
+        var newX = Mathf.Clamp(transform.position.x + inputMove.x * mouseSensitivity ,0,canvas.pixelRect.width);
+        var newY = Mathf.Clamp(transform.position.y + inputMove.y * mouseSensitivity,0,canvas.pixelRect.height);
         transform.position = new Vector2(newX, newY);
     }
 }
