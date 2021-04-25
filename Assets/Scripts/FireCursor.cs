cb5308a7e44e38a4d508710ae246ebecdbfe8709
Brenden <brendenbunker@gmail.com>
2021-04-25 00:17:28 -0600

    Clamping the cursor to the screen.

--- Assets/Scripts/FireCursor.cs
+++ Assets/Scripts/FireCursor.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class FireCursor : MonoBehaviour
 {
     public Canvas canvas;
+
+    void Start() {
+        canvas = GetComponentInParent<Canvas>();
+    }
     public void Move(Vector2 inputMove)
     {
-        var newX = Mathf.Clamp(transform.position.x + inputMove.x,0,10000);
-        var newY = transform.position.y + inputMove.y;
+        var newX = Mathf.Clamp(transform.position.x + inputMove.x,0,canvas.pixelRect.width);
+        var newY = Mathf.Clamp(transform.position.y + inputMove.y,0,canvas.pixelRect.height);
         transform.position = new Vector2(newX, newY);
     }
 }
